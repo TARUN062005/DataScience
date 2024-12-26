7245c9f02f5fa9d6ff2e9f18b6208eb36f62ebeb
TARUN062005 <princetarunvemuri@gmail.com>
2024-12-26 15:32:54 +0530

    new chapter

---
+++ LogisticRegressionScratch.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class LogisticRegressionScratch:
+    def __init__(self, learning_rate=0.01, n_iterations=1000):
+        self.lr = learning_rate
+        self.n_iter = n_iterations
+        self.weights = None
+        self.bias = None
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # Gradient Descent
+        for _ in range(self.n_iter):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_pred = self.sigmoid(linear_model)
+            dw = (1 / n_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / n_samples) * np.sum(y_pred - y)
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict_prob(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        return self.sigmoid(linear_model)
+
+    def predict(self, X, threshold=0.5):
+        probabilities = self.predict_prob(X)
+        return np.where(probabilities >= threshold, 1, 0)
